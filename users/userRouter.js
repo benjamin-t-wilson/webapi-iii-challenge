e42b25c66002f9c2bbadaae2ce355c4afedb01ca
benjamin-t-wilson <benjiimanwillson@gmail.com>
2019-11-05 20:37:49 -0700

    added post method for new user

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,19 @@
 const express = require('express');
 
+const db = require("./userDb")
+const postDb = require("../posts/postDb")
+
 const router = express.Router();
 
-router.post('/', (req, res) => {
-
+router.post('/', validateUser, (req, res) => {
+    const newUser = req.body
+db.insert(newUser)
+.then(user => {
+    res.status(201).json(user)
+})
+.catch(err => {
+    res.status(500).json({message: "Error creating new user", err})
+})
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -11,7 +21,13 @@ router.post('/:id/posts', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-
+db.get()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(err => {
+        res.status(500).json({message: "Error retrieving data", err})
+    })
 });
 
 router.get('/:id', (req, res) => {
@@ -33,15 +49,28 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
-
+    const { id } = req.params;
+    db.getById(id)
+      .then(post => {
+        post
+          ? ((req.post = post), next())
+          : res
+              .status(404)
+              .json({ message: `Post with id ${id} could not be found` });
+      })
+      .catch(err => {
+        res.status(500).json({ message: "Error retrieving data", error });
+      });
 };
 
 function validateUser(req, res, next) {
-
+    const bodyInfo = req.body;
+    bodyInfo.name ? next() : res.status(400).json({messsage: "Missing name on body"})
 };
 
 function validatePost(req, res, next) {
-
+    const bodyInfo = req.body;
+    bodyInfo.text ? next() : res.status(400).json({message: "Missing text on body"})
 };
 
 module.exports = router;
