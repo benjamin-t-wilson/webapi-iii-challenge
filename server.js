ef78d3c4a72884933b45344df45aea3018615bd2
benjamin-t-wilson <benjiimanwillson@gmail.com>
2019-11-05 19:17:40 -0700

    added server listener and logger

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-const express = 'express';
+const express = require("express");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.use(logger);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
-};
+  console.log(`${req.method} request to ${req.url} from ${req.get("host")}`);
+  next();
+}
 
 module.exports = server;
