8a3e05d5771f8afc16b3b49f8f8b247e312573d7
benjamin-t-wilson <benjiimanwillson@gmail.com>
2019-11-05 19:36:16 -0700

    finished getbyid middleware

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -13,8 +13,8 @@ db.get()
 })
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validatePostId, (req, res) => {
+    res.status(200).json(req.post)
 });
 
 router.delete('/:id', (req, res) => {
@@ -28,6 +28,17 @@ router.put('/:id', (req, res) => {
 // custom middleware
 
 function validatePostId(req, res, next) {
+    const { id } = req.params;
+    db.getById(id)
+    .then(post => {
+        post ? (
+            req.post = post,
+            next()
+        ) : res.status(404).json({message: `Post with id ${id} could not be found`})
+    })
+    .catch(err => {
+        res.status(500).json({message: "Error retrieving data", error})
+    })
 
 };
 
