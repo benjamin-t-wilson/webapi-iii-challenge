14108d9c771dc3a556a502f6db346265a70d328c
benjamin-t-wilson <benjiimanwillson@gmail.com>
2019-11-05 19:26:19 -0700

    fixed a routing error

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = express();
 
 server.use(logger);
 server.use("/api/users", userRouter)
-server.use("/api/users", postRouter)
+server.use("/api/posts", postRouter)
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
